9eedf0a94d2f20b53f2a3992b8b5ef4959080977
Juan Brito <123127481+juannbritto@users.noreply.github.com>
2023-06-02 11:38:32 -0300

    Add files via upload

---
+++ heranca_6/Advogado.java
@@ -0,0 +1,24 @@
+package heranca_6;
+
+import java.time.LocalDate;
+
+public class Advogado extends Funcionario{
+    
+    private String oab;
+
+    public Advogado(String oab, String cpf, String rg, String matricula, Setor setor, double salario, Genero genero, heranca_6.EstadoCivil EstadoCivil, LocalDate dataNascimento, int id, String nome, String telefone, String email, Endereco endereco) {
+        super(cpf, rg, matricula, setor, salario, genero, EstadoCivil, dataNascimento, id, nome, telefone, email, endereco);
+        this.oab = oab;
+    }
+
+    
+
+    @Override
+    public String toString() {
+        return "Advogado: " + 
+                super.toString() +
+                "OAB: " + oab;
+    }
+    
+    
+}
