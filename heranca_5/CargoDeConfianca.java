1071e7c5bac8daf0ecfd16e3fe3f9df129d77c60
Juan Brito <123127481+juannbritto@users.noreply.github.com>
2023-06-07 10:07:12 -0300

    Add files via upload

---
+++ heranca_5/CargoDeConfianca.java
@@ -0,0 +1,31 @@
+package heranca_5;
+
+import java.time.LocalDate;
+
+public abstract class CargoDeConfianca extends Funcionario{
+    
+    protected Bonificacao bonificacao;
+
+    public CargoDeConfianca(Bonificacao bonificacao, String nome, String cpf, String rg, Genero genero, Setor setor, double salarioBase, LocalDate dataAdmissão) {
+        super(nome, cpf, rg, genero, setor, salarioBase, dataAdmissão);
+        this.bonificacao = bonificacao;
+    }
+
+    public Bonificacao getBonificacao() {
+        return bonificacao;
+    }
+
+    public void setBonificacao(Bonificacao bonificacao) {
+        this.bonificacao = bonificacao;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\n Bonificacao: " + bonificacao + '}';
+    }
+    
+    
+    
+    
+}
---
+++ heranca_5/Genero.java
@@ -0,0 +1,28 @@
+package heranca_5;
+
+public enum Genero {
+    
+    MASCULINO("Masculino", 'M'),
+    FEMININO("Feminino", 'F');
+    
+    protected String texto;
+    protected char caracter;
+
+    private Genero(String texto, char caracter) {
+        this.texto = texto;
+        this.caracter = caracter;
+    }
+
+    
+    public String getTexto() {
+        return texto;
+    }
+
+    public char getCaracter() {
+        return caracter;
+    }
+    
+    
+    
+    
+}
